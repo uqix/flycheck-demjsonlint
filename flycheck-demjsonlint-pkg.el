f9170ff0364b4e03f6ec50695f4a9a694a1453fe
Zenkie Zhu <451218651@qq.com>
2016-11-13 19:26:46 +0800

    Delete -pkg.el file - MELPA doesn't need it

--- flycheck-demjsonlint-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "flycheck-demjsonlint" "0.1" "Flychecker for json-mode using jsonlint from demjson"
-  '((flycheck "30"))
-  :url "https://github.com/z4139jq/flycheck-demjsonlint"
-  :keywords '("convenience" "tools"))
